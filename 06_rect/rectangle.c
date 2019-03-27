a9c608319860ebab357ef296d965ef4fb39f7c58
Coursera Learner <nobody@nowhere.nul>
2019-03-27 22:15:17 +0000

    Did assignment 6

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,15 +16,53 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct rect {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rect rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.width < 0) {
+    r.x += r.width;
+    r.width = -r.width;
+  }
+  if (r.height < 0) {
+    r.y += r.height;
+    r.height = -r.height;
+  }
   return r;
 }
+
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
-  return r1;
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  rectangle r;
+  r.width = 0;
+  r.height = 0;
+  if (r1.x <= r2.x && r1.x + r1.width >= r2.x) {
+    r.x = r2.x;
+    r.width = min(r1.x + r1.width, r2.x + r2.width) - r.x;
+  }
+  else if (r1.x >= r2.x && r1.x <= r2.x + r2.width) {
+    r.x = r1.x;
+    r.width = min(r1.x + r1.width, r2.x + r2.width) - r.x;
+  }
+  else return r;
+  if (r1.y <= r2.y && r1.y + r1.height >= r2.y) {
+    r.y = r2.y;
+    r.height = min(r1.y + r1.height, r2.y + r2.height) - r.y;
+  }
+  else if (r1.y >= r2.y && r1.y <= r2.y + r2.height) {
+    r.y = r1.y;
+    r.height = min(r1.y + r1.height, r2.y + r2.height) - r.y;
+  }
+  else r.width = 0;
+  return r;
 }
 
 //You should not need to modify any code below this line
