ace84c00e1e95a373fe0389a4f3434dae6b24ba2
Coursera Learner <nobody@nowhere.nul>
2019-03-31 23:55:44 +0000

    Did project 1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,62 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  assert(c.value <= VALUE_ACE && c.value >= 2 && c.suit >= 0 && c.suit < 4);
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  switch(r) {
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+  case FULL_HOUSE: return "FULL_HOUSE";
+  case FLUSH: return "FLUSH";
+  case STRAIGHT: return "STRAIGHT";
+  case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
+  case TWO_PAIR: return "TWO_PAIR";
+  case PAIR: return "PAIR";
+  default: return "NOTHING";
+  }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  if (c.value <= 9 && c.value >= 2)
+    return c.value - '0';
+  if (c.value == 10)
+    return '0';
+  switch(c.value) {
+  case VALUE_ACE: return 'A';
+  case VALUE_KING: return 'K';
+  case VALUE_QUEEN: return 'Q';
+  default: return 'J';
+  }
 }
 
 
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch(c.suit) {
+  case SPADES: return 's';
+  case HEARTS: return 'h';
+  case DIAMONDS: return 'd';
+  default: return 'c';
+  }
 }
 
 void print_card(card_t c) {
-
+  printf("%c%c", value_letter(c), suit_letter(c));
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  temp.value = value_let;
+  temp.suit = suit_let;
+  assert_card_valid(temp);
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  temp.value = c % 13;
+  temp.suit = c / 13;
+  assert_card_valid(temp);
   return temp;
 }
